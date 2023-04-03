e427c31d220daf60601048d1874aedec9ea58002
nicholas-j-jackson <nicholas.jackson@ufl.edu>
2023-04-03 18:54:57 -0400

    Added routes

--- backend/routes/locationRoutes.js
+++ backend/routes/locationRoutes.js
@@ -1,23 +1,23 @@
 import {addNewLocation, getLocations, getLocationByTypeAndStoreNumber, getLocationsByTypeAndState, getLocationsByState, getLocationsByZip} from '../controllers/locationControllers.js'
 
 const locationRoutes = (app) => {
-    app.route('/locations')
+    app.route('/api/locations')
         // GET endpoint
         .get(getLocations)
     
         // POST Endpoint
             .post(addNewLocation);
 
-    app.route('/locations/type_id/:type/:store_id')
+    app.route('/api/locations/type_id/:type/:store_id')
         .get(getLocationByTypeAndStoreNumber);
     
-    app.route('/locations/type_state/:type/:state')
+    app.route('/api/locations/type_state/:type/:state')
         .get(getLocationsByTypeAndState);
     
-    app.route('/locations/state/:state')
+    app.route('/api/locations/state/:state')
         .get(getLocationsByState);
 
-    app.route('/locations/zip/:zip')
+    app.route('/api/locations/zip/:zip')
         .get(getLocationsByZip);
 }
 
--- backend/routes/priceRoutes.js
+++ backend/routes/priceRoutes.js
@@ -1,17 +1,17 @@
 import {addNewPrice, getPrices, getPriceByStoreNumberAndProduct, getAllPricesFromStore} from '../controllers/priceControllers.js'
 
 const priceRoutes = (app) => {
-    app.route('/prices')
+    app.route('/api/prices')
         // GET endpoint
         .get(getPrices)
     
         // POST Endpoint
             .post(addNewPrice);
 
-    app.route('/prices/type_id_item/:type/:store_id/:item')
+    app.route('/api/prices/type_id_item/:type/:store_id/:item')
         .get(getPriceByStoreNumberAndProduct);
 
-    app.route('/prices/type_id/:type/:store_id')
+    app.route('/api/prices/type_id/:type/:store_id')
         .get(getAllPricesFromStore);
     
 }
