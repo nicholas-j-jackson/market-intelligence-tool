fed6ac7ae3fe89de3dbe47fca5771072110ee00a
nicholas-j-jackson <nicholas.jackson@ufl.edu>
2023-04-04 14:47:08 -0400

    simplified index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import './bootstrap.min.css';
+import ReactDOM from 'react-dom';
 import App from './Components/App';
 
-import { Provider } from 'react-redux';
-import store from './store';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Provider store={store}>
-    <App />
-    </Provider>
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
