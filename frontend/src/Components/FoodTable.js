a4f4d56f86e228460c8e59a46a27a5900f7b6992
nicholas-j-jackson <nicholas.jackson@ufl.edu>
2023-04-04 14:45:04 -0400

    Created table to display food items for the selected restaurant

--- frontend/src/Components/FoodTable.js
+++ frontend/src/Components/FoodTable.js
@@ -1,21 +1,34 @@
-import Table from 'react-bootstrap/Table';
+import {Table } from 'react-bootstrap';
 
 
-function FoodTable() {
+const FoodTable = (props) => {
     return (
 
         <Table striped bordered hover responsive>
 
             <thead>
                 <tr>
-                    <th>Item</th>
-                    <th>Size</th>
-                    <th>Price</th>
+                    <th width='40%'>Item</th>
+                    <th width='30%'>Size</th>
+                    <th width='30%'>Price</th>
                 </tr>
             </thead>
 
             <tbody>
-                
+                {props.foods.map((food) => {
+                    if (food.address ===  props.location.split(', ')[1])
+                    {
+                        return (
+                        <tr>
+                            <td width='40%'> {food.name}</td>
+                            <td width='30%'> {food.size} </td>
+                            <td width='30%'> {food.price}</td>
+
+                        </tr>
+                        )
+                    }
+                    
+                })}        
             </tbody>
 
         </Table>
