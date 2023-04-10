31e6eb74eb049e7e6853c553a7358cb420dfa282
nicholas-j-jackson <nicholas.jackson@ufl.edu>
2023-04-09 22:11:00 -0400

    modified to allow for routes

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,237 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Container, NavItem} from 'react-bootstrap';
 import axios from 'axios';
 import {Row, Col, Form} from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import FoodTable from './FoodTable';
 import SelectRestaurantForm from './selectRestaurantForm';
 import Header from './Header';
+import HomeScreen from './HomeScreen';
 
-
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      locations: [],
-
-        mapping: [{name: 'Jimmy Johns', type:'john'},
-                    {name: 'FireHouse Subs', type:'fire'},
-                    {name: 'Jersey Mike\'s', type:'mike'}],
-
-        foods: [],
-
-        chain1: 'Jimmy Johns',
-        chain2: 'FireHouse Subs',
-        chain3: 'Jersey Mike\'s',
-        chains: ['Jimmy Johns', 'FireHouse Subs', 'Jersey Mike\'s'],
-
-        location1: '',
-        location2: '',
-        location3: '',
-
-        location1Foods: [],
-        location2Foods: [],
-        location3Foods: [],
-
-
-    }
-
-    this.updateLocation1 = this.updateLocation1.bind(this);
-    this.updateLocation2 = this.updateLocation2.bind(this);
-    this.updateLocation3 = this.updateLocation3.bind(this);
-    
-  }
-
-    updateLocation1 = (location) => {
-        this.setState({location1: location});
-    
-        let type = this.state.mapping.find(x => x.name === this.state.chain1).type;
-        let newLocation = this.state.locations.find(x => x.address === location.split(',')[0]);
-        const url = `http://localhost:3001/api/prices/type_id/${type}/` + newLocation.store_id;
-        axios.get(url,).then(response => response.data)
-        .then((data) => {
-            if (type === 'john'){
-                let newFoods = data.map(x => {
-                    return {item: x.item, size: x.size, price: x["price "]}
-                })
-                this.setState({ location1Foods: newFoods })
-            }
-            else{
-                this.setState({ location1Foods: data })
-            }
-            //console.log(this.state.location1Foods)
-        })
-
-    }
-
-    updateLocation2 = (location) => {
-        this.setState({location2: location});
-
-        let type = this.state.mapping.find(x => x.name === this.state.chain2).type;
-        let newLocation = this.state.locations.find(x => x.address === location.split(',')[0]);
-        const url = `http://localhost:3001/api/prices/type_id/${type}/` + newLocation.store_id;
-        axios.get(url,).then(response => response.data)
-        .then((data) => {
-            if (type === 'john'){
-                let newFoods = data.map(x => {
-                    return {item: x.item, size: x.size, price: x["price "]}
-                })
-                this.setState({ location2Foods: newFoods })
-            }
-            else{
-                this.setState({ location2Foods: data })
-            }            //console.log(this.state.location2Foods)
-        })
-
-    }
-
-    updateLocation3 = (location) => {
-        this.setState({location3: location});
-
-        let type = this.state.mapping.find(x => x.name === this.state.chain3).type;
-        let newLocation = this.state.locations.find(x => x.address === location.split(',')[0]);
-        const url = `http://localhost:3001/api/prices/type_id/${type}/` + newLocation.store_id;
-        axios.get(url,).then(response => response.data)
-        .then((data) => {
-            if (type === 'john'){
-                let newFoods = data.map(x => {
-                    return {item: x.item, size: x.size, price: x["price "]}
-                })
-                this.setState({ location3Foods: newFoods })
-            }
-            else{
-                this.setState({ location3Foods: data })
-            }
-        })
-
-    }
-
-
-
-  componentDidMount() {
-      const url = 'http://localhost:3001/api/locations/state/FL';
-      axios.get(url,).then(response => response.data)
-      .then((data) => {
-        this.setState({ locations: data })
-        console.log(this.state.locations)
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
-
-  }  
-
-  render() {
+const App = () => {
     return (
-      <Container fluid expand="xl">
-
-        <Header />
-
-        <Row>
-            <Col>
-            <Form> 
-                <Form.Group controlId="exampleForm.SelectCustom">
-                <Form.Label>Select Chain to Compare</Form.Label>
-                <Form.Control as="select" custom onChange={e => this.setState({chain1: e.target.value})}>
-                    {this.state.chains.map((chain, index) => {
-                        return <option key={index}>{chain}</option>
-                    })}
-                    
-                    </Form.Control>
-                </Form.Group>
-            </Form>
-            </Col>
-
-            <Col>
-            <Form> 
-                <Form.Group controlId="exampleForm.SelectCustom">
-                <Form.Label>Select Chain to Compare</Form.Label>
-                <Form.Control as="select" custom onChange={e => this.setState({chain2: e.target.value})}>
-                    {this.state.chains.map((chain, index) => {
-                        return <option key={index}>{chain}</option>
-                    })}
-                    </Form.Control>
-                </Form.Group>
-            </Form>
-            </Col>
-
-            <Col>
-            <Form> 
-                <Form.Group controlId="exampleForm.SelectCustom">
-                <Form.Label>Select Chain to Compare</Form.Label>
-                <Form.Control as="select" custom onChange={e => this.setState({chain3: e.target.value})}>
-                    {this.state.chains.map((chain, index) => {
-                        return <option key={index}>{chain}</option>
-                    })}
-                    </Form.Control>
-                </Form.Group>
-            </Form>
-            </Col>
-        </Row>
-
-
-        <Row>
-
-            <Col>
-            <SelectRestaurantForm locations={this.state.locations.filter(location => location.type === (this.state.mapping.filter(mapping => mapping.name === this.state.chain1).map(mapping => mapping.type)[0]))}
-                                    chains={this.state.chains}
-                                    chain={this.state.chain1}
-                                    updateLocation={this.updateLocation1}
-                                    location={this.state.location1}
-                                    />
-
-            </Col>
-
-            <Col>
-            <SelectRestaurantForm locations={this.state.locations.filter(location => location.type === (this.state.mapping.filter(mapping => mapping.name === this.state.chain2).map(mapping => mapping.type)[0]))}
-                                    chains={this.state.chains}
-                                    chain={this.state.chain2}
-                                    updateLocation={this.updateLocation2}
-                                    location={this.state.location2}
-                                    />
-            </Col>
-
-            <Col>
-            <SelectRestaurantForm locations={this.state.locations.filter(location => location.type === (this.state.mapping.filter(mapping => mapping.name === this.state.chain3).map(mapping => mapping.type)[0]))}
-                                    chains={this.state.chains}
-                                    chain={this.state.chain3}
-                                    updateLocation={this.updateLocation3}
-                                    location={this.state.location3}
-                                    />
-            </Col>
-
-        </Row>
-            
-        <Row>
-            <Col>
-                <FoodTable location={this.state.location1} 
-                            foods={this.state.location1Foods}
-                />
-            </Col>
-
-            <Col>
-                <FoodTable location={this.state.location2}
-                            foods={this.state.location2Foods}
-                />
-            </Col>
-            
-            <Col>
-            <FoodTable location={this.state.location3}
-                        foods={this.state.location3Foods}
-            />
-            </Col>
-
-        </Row>
-
-        
-    </Container>
-
+        <Container fluid expand="xl">
+            <Header />
+            <Fragment>
+                <Router>
+                    <Routes>
+                        <Route exact path="/" element={<HomeScreen/>}  />
+                        <Route path="/review" element={<div>review</div>} />
+                    </Routes>
+                </Router>
+            </Fragment>
+        </Container>
     );
-} 
-}
-
+};
 
 export default App;
