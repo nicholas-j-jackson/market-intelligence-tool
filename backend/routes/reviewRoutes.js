4722491115547d7fdb2e83d56d31e9ec2925a40e
nicholas-j-jackson <nicholas.jackson@ufl.edu>
2023-04-09 23:34:55 -0400

    added /api/ to review Routes

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -3,12 +3,12 @@ import {getReviewsByCity, getReviewsBybizId} from '../controllers/reviewControll
 const reviewRoutes = (app) => {
 
     // Create route to get all reviews for a specific city
-    app.route('/reviews/city/:city')
+    app.route('/api/reviews/city/:city')
         .get(getReviewsByCity)
     
 
     // Create route to get all reviews for a specific bizId
-    app.route('/reviews/bizId/:bizId')
+    app.route('/api/reviews/bizId/:bizId')
         .get(getReviewsBybizId);
     
     // There is no POST Endpoint as all data is added by the admin
